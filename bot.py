7c610963b84c09d5af3a19a480f51fd9f9c4f4d1
DimonLimon12332 <156221143+DimonLimon12332@users.noreply.github.com>
2024-02-04 13:55:39 +0600

    Create bot.py
    Что то

---
+++ bot.py
@@ -0,0 +1,32 @@
+import discord
+import random
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Привет! Я бот {bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+@bot.command()
+async def random_pass(ctx, pass_length = 8):
+    characters = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+    zhocko = ""
+
+    for _ in range(pass_length):
+        zhocko += random.choice(characters)
+
+    await ctx.send(zhocko)
+
+bot.run("Токена тут нет...")
